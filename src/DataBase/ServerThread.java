0009b1f155df4d51c0e261a364cf7a66815b12bc
Tan3103 <tannur.uataev@mail.ru>
2022-05-09 23:32:54 +0600

    server thread update

--- src/DataBase/ServerThread.java
+++ src/DataBase/ServerThread.java
@@ -26,18 +26,17 @@ public class ServerThread extends Thread  {
                     Customer customer = packageData.getVisitor();
                     manager.addCustomer(customer);
                 }
-                /*
-                else if(packageData.getOperationType().equals("ADDGuitar")){
+                else if(packageData.getOperationType().equals("ADD S")){
                     Smartphone smartphone = packageData.getGuitar();
-                    manager.addGuitar(smartphone);
+                    manager.addSmartphone(smartphone);
                 }
-                else if(packageData.getOperationType().equals("ADDKeyboard")){
+                else if(packageData.getOperationType().equals("ADD L")){
                     Laptop laptop = packageData.getKeyboardInstrument();
-                    manager.addKeyboard(laptop);
+                    manager.addLaptop(laptop);
                 }
-                else if(packageData.getOperationType().equals("ADDPercussion")){
+                else if(packageData.getOperationType().equals("ADD P")){
                     PhotoCamera photoCamera = packageData.getPercussionInstrument();
-                    manager.addPercussion(photoCamera);
+                    manager.addPhotoCamera(photoCamera);
                 }
                 else if(packageData.getOperationType().equals("LIST")){
                     ArrayList<Customer> arrayCustomer = manager.getAllVisitor();
@@ -45,96 +44,54 @@ public class ServerThread extends Thread  {
                     toVisitor.setVisitors(arrayCustomer);
                     outputStream.writeObject(toVisitor);
                 }
-                else if(packageData.getOperationType().equals("LIST Guitar")){
-                    ArrayList<Smartphone> arraySmartphone = manager.getAllGuitar();
+                else if(packageData.getOperationType().equals("LIST S")){
+                    ArrayList<Smartphone> arraySmartphone = manager.getAllSmartphone();
                     PackageData toGuitar = new PackageData();
                     toGuitar.setGuitars(arraySmartphone);
                     outputStream.writeObject(toGuitar);
                 }
-                else if(packageData.getOperationType().equals("LIST Keyboard")){
-                    ArrayList<Laptop> arrayKeyboard = manager.getAllKeyboard();
+                else if(packageData.getOperationType().equals("LIST L")){
+                    ArrayList<Laptop> arrayKeyboard = manager.getAllLaptop();
                     PackageData toKeyboard = new PackageData();
                     toKeyboard.setKeyboardInstruments(arrayKeyboard);
                     outputStream.writeObject(toKeyboard);
                 }
-                else if(packageData.getOperationType().equals("LIST Percussion")){
-                    ArrayList<PhotoCamera> arrayPercussion = manager.getAllPercussion();
+                else if(packageData.getOperationType().equals("LIST P")){
+                    ArrayList<PhotoCamera> arrayPercussion = manager.getAllPhotoCamera();
                     PackageData toPercussion = new PackageData();
                     toPercussion.setPercussionInstruments(arrayPercussion);
                     outputStream.writeObject(toPercussion);
                 }
-                else if(packageData.getOperationType().equals("Delete Visitor")){
+                else if(packageData.getOperationType().equals("Delete Customer")){
                     int idServer = packageData.getId();
-                    manager.deleteVisitors(idServer);
+                    manager.deleteCustomer(idServer);
                 }
-                else if(packageData.getOperationType().equals("Delete Guitar")){
+                else if(packageData.getOperationType().equals("Delete Product")){
                     int idServer = packageData.getId();
-                    manager.deleteGuitar(idServer);
+                    manager.deleteProduct(idServer);
                 }
-                else if(packageData.getOperationType().equals("Delete Keyboard")){
+                else if(packageData.getOperationType().equals("Update Product")){
                     int idServer = packageData.getId();
-                    manager.deleteKeyboard(idServer);
+                    manager.updateProduct(idServer);
                 }
-                else if(packageData.getOperationType().equals("Delete Percussion")){
-                    int idServer = packageData.getId();
-                    manager.deletePercussion(idServer);
-                }
-                else if(packageData.getOperationType().equals("Update Guitar")){
-                    int idServer = packageData.getId();
-                    manager.updateGuitar(idServer);
-                }
-                else if(packageData.getOperationType().equals("Update Keyboard")){
-                    int idServer = packageData.getId();
-                    manager.updateKeyboard(idServer);
-                }
-                else if(packageData.getOperationType().equals("Update Percussion")){
-                    int idServer = packageData.getId();
-                    manager.updatePercussion(idServer);
-                }
-                else if(packageData.getOperationType().equals("Find Guitar")){
-                    String findGuitar = manager.findGuitar(packageData.getFind());
+                else if(packageData.getOperationType().equals("Find S")){
+                    String findGuitar = manager.findSmartphone(packageData.getFind());
                     PackageData toPercussion = new PackageData();
                     toPercussion.setFind(findGuitar);
                     outputStream.writeObject(toPercussion);
                 }
-                else if(packageData.getOperationType().equals("Find Keyboard")){
-                    String findGuitar = manager.findKeyboard(packageData.getFind());
+                else if(packageData.getOperationType().equals("Find L")){
+                    String findGuitar = manager.findLaptop(packageData.getFind());
                     PackageData toPercussion = new PackageData();
                     toPercussion.setFind(findGuitar);
                     outputStream.writeObject(toPercussion);
                 }
-                else if(packageData.getOperationType().equals("Find Percussion")){
-                    String findGuitar = manager.findPercussion(packageData.getFind());
+                else if(packageData.getOperationType().equals("Find P")){
+                    String findGuitar = manager.findPhotoCamera(packageData.getFind());
                     PackageData toPercussion = new PackageData();
                     toPercussion.setFind(findGuitar);
                     outputStream.writeObject(toPercussion);
                 }
-                else if(packageData.getOperationType().equals("Find Type Guitar")){
-                    String findGuitar = manager.findTypeGuitar(packageData.getFind());
-                    PackageData toPercussion = new PackageData();
-                    toPercussion.setFind(findGuitar);
-                    outputStream.writeObject(toPercussion);
-                }
-                else if(packageData.getOperationType().equals("Find Type Keyboard")){
-                    String findGuitar = manager.findTypeKeyboard(packageData.getFind());
-                    PackageData toPercussion = new PackageData();
-                    toPercussion.setFind(findGuitar);
-                    outputStream.writeObject(toPercussion);
-                }
-                else if(packageData.getOperationType().equals("Find Type Percussion")){
-                    String findGuitar = manager.findTypePercussion(packageData.getFind());
-                    PackageData toPercussion = new PackageData();
-                    toPercussion.setFind(findGuitar);
-                    outputStream.writeObject(toPercussion);
-                }
-                else if(packageData.getOperationType().equals("LIST Discount")){
-                    ArrayList<Product> arrayPercussion = manager.getAllDiscount();
-                    PackageData toPercussion = new PackageData();
-                    toPercussion.setProducts(arrayPercussion);
-                    outputStream.writeObject(toPercussion);
-                }
-
-                 */
             }
             inputStream.close();
             outputStream.close();
